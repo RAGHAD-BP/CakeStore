c72a77887beff1b7611cdc0fc39d9c1cbf57c37d
Raghad Alsuwayhi <173504653+RAGHAD-BP@users.noreply.github.com>
2026-05-15 01:44:08 +0300

    Add title to Menu component and improve layout styling

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -1,10 +1,14 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import {basicMenu} from './basicMenu';
+
 
 
 function Menu() {
   return (
     <View style={style.Page} >
+      <View style={style.title}>
+      Menu
+      </View>
         {/*هذي تمشي على الاريي الاساسيه وتدخل كل اوبجيت*/}
       {basicMenu.map((menu, index) => (
         <View key={index}>
@@ -18,7 +22,7 @@ function Menu() {
                   {item.name} - ${item.price}
                 </Text>
               ))}
-            </View>
+            </View>   
           ))}
         </View>
       ))}
@@ -36,6 +40,13 @@ const style = StyleSheet.create({
         flexDirection:'row',
         flexWrap:'wrap',
     }, 
+    title:{
+        width:'100%',
+        padding:10,
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign:'center', 
+    },
     Type:{
         padding:10,
         gap:2,
